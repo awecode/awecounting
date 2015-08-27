104eada95d180d9e2f456d98b56ed435b6122a16
Roshan Shrestha <roshanshrestha01@gmail.com>
2015-08-27 16:32:06 +0545

    rest_framework in installed_apps

--- inventory/serializer.py
+++ inventory/serializer.py
@@ -1,5 +1,17 @@
 from rest_framework import serializers
-from inventory.models import Purchase, PurchaseRow
+from inventory.models import Purchase, PurchaseRow, Item, Party
+
+class ItemSerializer(serializers.ModelSerializer):
+	unit_id = serializers.ReadOnlyField(source='unit.id')
+
+	class Meta:
+		model = Item
+		exclude = ['unit']
+
+class PartySerializer(serializers.ModelSerializer):
+
+	class Meta:
+		model = Party
 
 class PurchaseRowSerializer(serializers.ModelSerializer):
     item_id = serializers.ReadOnlyField(source='item.id')
