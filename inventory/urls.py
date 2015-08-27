edb968bc4100a0af1ede0f7146a01b33a49ac410
Roshan Shrestha <roshanshrestha01@gmail.com>
2015-08-27 16:33:31 +0545

    Item and Party rest api

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,6 +1,14 @@
 from django.conf.urls import patterns, url
 import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = patterns('',
                        url(r'^purchase/create/$', views.create_purchase, name='purchase-create'),
+
+                       # djangorestframework api
+                       url(r'^api/item/$', views.ItemList.as_view()),
+                       url(r'^api/parties/$', views.PartyList.as_view()),
+
 )
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- inventory/views.py
+++ inventory/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from inventory.models import Purchase, Purchase, PurchaseRow
+from inventory.models import Item, Purchase, PurchaseRow, Party
 from inventory.forms import ItemForm
-from inventory.serializer import PurchaseSerializer
+from inventory.serializer import PurchaseSerializer, ItemSerializer,PartySerializer
 import datetime
+from rest_framework import generics
+
 def create_purchase(request, id=None):
     if id:
         purchase = get_object_or_404(Purchase, id=id)
@@ -12,3 +14,17 @@ def create_purchase(request, id=None):
         scenario = 'Create'
     data = PurchaseSerializer(purchase).data
     return render(request, 'create-purchase.html', {'data': data, 'scenario': scenario, 'purchase': purchase})
+
+
+
+# djangorestframework API
+
+class ItemList(generics.ListCreateAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+
+class PartyList(generics.ListCreateAPIView):
+    queryset = Party.objects.all()
+    serializer_class = PartySerializer
+
+
