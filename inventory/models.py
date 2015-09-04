9078559d6cb5ac70a5e96bc37b237d699d20a095
Roshan Shrestha <roshanshrestha01@gmail.com>
2015-09-04 12:17:34 +0545

    get_voucher_no added in PurchaseRow and SaleRow

--- inventory/models.py
+++ inventory/models.py
@@ -209,10 +209,14 @@ class PurchaseRow(models.Model):
     unit = models.ForeignKey(Unit)
     purchase = models.ForeignKey(Purchase, related_name='rows')
 
+    def get_voucher_no(self):
+        return self.purchase.voucher_no
+
 class Sale(models.Model):
     party = models.ForeignKey(Party, blank=True, null=True)
     voucher_no = models.PositiveIntegerField(blank=True, null=True)
     date = models.DateField(default=datetime.datetime.today)
+
 
 class SaleRow(models.Model):
     sn = models.PositiveIntegerField()
@@ -223,6 +227,8 @@ class SaleRow(models.Model):
     unit = models.ForeignKey(Unit)
     sale = models.ForeignKey(Sale, related_name='rows')
 
+    def get_voucher_no(self):
+        return self.sale.voucher_no
 # class InventroyAccount(models.Model):
 #     item = models.ForeignKey(Item)
 #     dr_amount = models.FloatField(null=True, blank=True)
