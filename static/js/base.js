29377a62bda790fcb2227b38b9dc025585452e46
Roshan Shrestha <roshanshrestha01@gmail.com>
2015-09-21 16:52:41 +0545

    flip ui

--- static/js/base.js
+++ static/js/base.js
@@ -1548,3 +1548,22 @@ function HashTable() {
     }
 
 }
+
+$(document).ready(function () {
+    $(function () {
+
+        $('.flip-container').mouseenter(function () {
+            $(this).addClass('open');
+        });
+        $('.flip-container').mouseleave(function () {
+            $(this).removeClass('open');
+        });
+
+        $('.btn.warning, .btn-danger').click(function (e) {
+            var action = e.currentTarget.value.toLowerCase();
+            if (confirm('Are you sure you want to ' + action + '?')) {
+                return true;
+            } else return false;
+        });
+    });
+});
