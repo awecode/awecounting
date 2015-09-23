81473bdf2f9aa5e038c3cbd6da659ff5294e6c8c
Dipesh Acharya <xtranophilist@gmail.com>
2015-09-23 15:56:45 +0545

    Ledger app and models

---
+++ app/lib.py
@@ -0,0 +1,12 @@
+def zero_for_none(obj):
+    if obj is None:
+        return 0
+    else:
+        return obj
+
+
+def none_for_zero(obj):
+    if not obj:
+        return None
+    else:
+        return obj
