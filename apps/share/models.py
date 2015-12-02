7b9e5feb616f29577c8a3fdf6c2ec320570b6149
Roshan Shrestha <roshanshrestha01@gmail.com>
2015-12-02 16:49:26 +0545

    company property in share app

--- apps/share/models.py
+++ apps/share/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import datetime
+from apps.users.models import Company
 
 
 class ShareHolder(models.Model):
@@ -7,6 +8,7 @@ class ShareHolder(models.Model):
     phone_no = models.CharField(max_length=50, blank=True, null=True)
     address = models.TextField(blank=True, null=True)
     email = models.EmailField(blank=True, null=True)
+    company = models.ForeignKey(Company)
 
     # TODO
     # Create ledger/account on creation of shareholder
@@ -17,6 +19,7 @@ class Collection(models.Model):
     interest_rate = models.FloatField()
     start_date = models.DateField(blank=True, null=True)
     end_date = models.DateField(blank=True, null=True)
+    company = models.ForeignKey(Company)
 
 
 class Investment(models.Model):
@@ -24,3 +27,4 @@ class Investment(models.Model):
     date = models.DateField(default=datetime.date.today)
     amount = models.FloatField()
     collection = models.ForeignKey(Collection)
+    company = models.ForeignKey(Company)
