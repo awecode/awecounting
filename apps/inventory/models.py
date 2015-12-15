473a1e3f1c5630482b12670934b10db07b0dcb46
Roshan Shrestha <roshanshrestha01@gmail.com>
2015-12-15 12:20:47 +0545

    get_next_voucher_noi() moved to helpers.py

--- apps/inventory/models.py
+++ apps/inventory/models.py
@@ -8,16 +8,8 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.db.models import F
 from apps.ledger.models import Account
 from apps.users.models import Company
+from awecounting.utils.helpers import get_next_voucher_no
 
-
-def get_next_voucher_no(cls, attr):
-    from django.db.models import Max
-
-    max_voucher_no = cls.objects.all().aggregate(Max(attr))[attr + '__max']
-    if max_voucher_no:
-        return max_voucher_no + 1
-    else:
-        return 1
 
 def none_for_zero(obj):
     if not obj:
--- awecounting/utils/helpers.py
+++ awecounting/utils/helpers.py
@@ -10,3 +10,12 @@ def none_for_zero(obj):
         return None
     else:
         return obj
+
+def get_next_voucher_no(cls, attr):
+    from django.db.models import Max
+
+    max_voucher_no = cls.objects.all().aggregate(Max(attr))[attr + '__max']
+    if max_voucher_no:
+        return max_voucher_no + 1
+    else:
+        return 1
