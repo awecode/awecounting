828917bb84af6c76d63ce43827d8110098c686f8
Roshan Shrestha <roshanshrestha01@gmail.com>
2015-12-16 16:33:48 +0545

    journal_voucher.js created

--- apps/account/views.py
+++ apps/account/views.py
@@ -3,17 +3,21 @@ from django.core.urlresolvers import reverse_lazy
 from django.views.generic import ListView
 from awecounting.utils.mixins import DeleteView, UpdateView, CreateView, AjaxableResponseMixin
 from .models import JournalVoucher, JournalVoucherRow
-
+from .forms import JournalVoucherForm
 
 class JournalVoucherView(object):
 	model = JournalVoucher
 	success_url = reverse_lazy('account:journalvoucher_list')
-
+	form_class = JournalVoucherForm
 
 class JournalVoucherList(JournalVoucherView, ListView):
 	pass
 
 
+class JournalVoucherCreate(JournalVoucherView, CreateView):
+	pass
+
+
 def journalvoucher_create(request):
-	return render(request, 'account/journalvoucher_create.html')
+	return render(request, 'account/journalvoucher_form.html')
 # Create your views here.
