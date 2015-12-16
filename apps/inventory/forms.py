f715d121c564aa18016f8434c2e8c70fdeab7496
Dipesh Acharya <xtranophilist@gmail.com>
2015-12-16 13:57:49 +0545

    mr

--- apps/inventory/forms.py
+++ apps/inventory/forms.py
@@ -6,38 +6,6 @@ from apps.inventory.models import Item, Party, Unit, InventoryAccount, UnitConve
 from awecounting.utils.forms import HTML5BootstrapModelForm, KOModelForm
 
 
-class KOModelForm(forms.ModelForm):
-    class EmailTypeInput(forms.widgets.TextInput):
-        input_type = 'email'
-
-    class NumberTypeInput(forms.widgets.TextInput):
-        input_type = 'number'
-
-    class TelephoneTypeInput(forms.widgets.TextInput):
-        input_type = 'tel'
-
-    class DateTypeInput(forms.widgets.DateInput):
-        input_type = 'date'
-
-    class DateTimeTypeInput(forms.widgets.DateTimeInput):
-        input_type = 'datetime'
-
-    class TimeTypeInput(forms.widgets.TimeInput):
-        input_type = 'time'
-
-    def __init__(self, *args, **kwargs):
-        super(KOModelForm, self).__init__(*args, **kwargs)
-        self.refine()
-
-    def refine(self):
-        for (name, field) in self.fields.items():
-            # add HTML5 required attribute for required fields
-            if field.required:
-                field.widget.attrs['required'] = 'required'
-            field.widget.attrs['data-bind'] = 'value: ' + name
-            field.widget.attrs['class'] = 'form-control'
-
-
 class ItemForm(KOModelForm, TranslationModelForm):
     account_no = forms.Field(widget=forms.TextInput(), label=_('Inventory Account No.'))
 
--- awecounting/utils/forms.py
+++ awecounting/utils/forms.py
@@ -131,6 +131,8 @@ class HTML5BootstrapModelForm(HTML5ModelForm):
 
 
 class KOModelForm(forms.ModelForm):
+    required_css_class = 'required'
+    
     def __init__(self, *args, **kwargs):
         super(KOModelForm, self).__init__(*args, **kwargs)
         self.refine_for_ko()
