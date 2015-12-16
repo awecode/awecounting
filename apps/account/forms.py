a4ca77225d1820d38ac3b7ff5068ed84069b5f9a
Roshan Shrestha <roshanshrestha01@gmail.com>
2015-12-16 16:32:28 +0545

    JournalVoucherForm created

--- apps/account/forms.py
+++ apps/account/forms.py
@@ -1,10 +1,10 @@
 from django.core.urlresolvers import reverse_lazy
 from awecounting.utils.forms import HTML5BootstrapModelForm
-# from .models import JournalVoucher, JournalVoucherRow
+from .models import JournalVoucher, JournalVoucherRow
 
-# class JournalVoucherForm(HTML5BootstrapModelForm):
-# 	class Meta:
-# 		model = JournalVoucher
-# 		fields = '__all__'
+class JournalVoucherForm(HTML5BootstrapModelForm):
+	class Meta:
+		model = JournalVoucher
+		fields = '__all__'
 
 
