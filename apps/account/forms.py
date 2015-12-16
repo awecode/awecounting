925a0465d91b4e4905571db232418eaf91be5756
Roshan Shrestha <roshanshrestha01@gmail.com>
2015-12-16 14:48:22 +0545

    journal voucher list

---
+++ apps/account/forms.py
@@ -0,0 +1,10 @@
+from django.core.urlresolvers import reverse_lazy
+from awecounting.utils.forms import HTML5BootstrapModelForm
+# from .models import JournalVoucher, JournalVoucherRow
+
+# class JournalVoucherForm(HTML5BootstrapModelForm):
+# 	class Meta:
+# 		model = JournalVoucher
+# 		fields = '__all__'
+
+
--- apps/account/views.py
+++ apps/account/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import ListView
+from awecounting.utils.mixins import DeleteView, UpdateView, CreateView, AjaxableResponseMixin
+from .models import JournalVoucher, JournalVoucherRow
+
+class JournalVoucherView(object):
+	model = JournalVoucher
+	success_url = reverse_lazy('account:journalvoucher_list')
+
+class JournalVoucherList(JournalVoucherView, ListView):
+	pass
+
 
 # Create your views here.
