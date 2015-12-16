c7c7bf6c3afdae88824b34692e27db108b4bc96a
Roshan Shrestha <roshanshrestha01@gmail.com>
2015-12-16 15:44:01 +0545

    journalvoucher_create

--- apps/account/urls.py
+++ apps/account/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = patterns('',
                        url(r'^journalvoucher/$', views.JournalVoucherList.as_view(), name='journalvoucher_list'),
-                       # url(r'^shareholder/add/$', views.ShareHolderCreate.as_view(), name='shareholder_add'),
+                       url(r'^journalvoucher/add/$', views.journalvoucher_create, name='journalvoucher_add'),
                        # url(r'^shareholder/edit/(?P<pk>\d+)/$', views.ShareHolderUpdate.as_view(), name='shareholder_edit'),
                        # url(r'^shareholder/delete/(?P<pk>\d+)/$', views.ShareHolderDelete.as_view(), name='shareholder_delete'),
                        )
--- apps/account/views.py
+++ apps/account/views.py
@@ -1,14 +1,19 @@
+from django.shortcuts import render
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic import ListView
 from awecounting.utils.mixins import DeleteView, UpdateView, CreateView, AjaxableResponseMixin
 from .models import JournalVoucher, JournalVoucherRow
 
+
 class JournalVoucherView(object):
 	model = JournalVoucher
 	success_url = reverse_lazy('account:journalvoucher_list')
+
 
 class JournalVoucherList(JournalVoucherView, ListView):
 	pass
 
 
+def journalvoucher_create(request):
+	return render(request, 'account/journalvoucher_create.html')
 # Create your views here.
