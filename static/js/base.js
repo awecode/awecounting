23eea04e751426cd521b972fe672f7c25312e870
Dipesh Acharya <xtranophilist@gmail.com>
2015-12-16 15:43:42 +0545

    Bootstrap multiple modal fix

--- static/js/base.js
+++ static/js/base.js
@@ -1640,3 +1640,10 @@ $(document).ready(function () {
     });
 });
 
+$(document).on('show.bs.modal', '.modal', function () {
+    var zIndex = 1040 + (10 * $('.modal:visible').length);
+    $(this).css('z-index', zIndex);
+    setTimeout(function () {
+        $('.modal-backdrop').not('.modal-stack').css('z-index', zIndex - 1).addClass('modal-stack');
+    }, 0);
+});
