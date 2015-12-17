12f6c985fc2631267f95ba8a1c6af1f535745ef7
Roshan Shrestha <roshanshrestha01@gmail.com>
2015-12-17 15:00:51 +0545

    JournalVoucherVM observable

--- static/js/account/journalvoucher_form.js
+++ static/js/account/journalvoucher_form.js
@@ -5,5 +5,26 @@ $(document).ready(function () {
 });
 
 function JournalVoucherViewModel(){
-	
+	var self = this
+
+	self.id =  ko.observable();
+	self.voucher_no = ko.observable();
+	self.date = ko.observable();
+	self.narration = ko.observable();
+	self.status = ko.observable();
+
+    self.table_view = new TableViewModel({rows: data.rows}, JournalVoucherRowViewModel);
+
+}
+
+function JournalVoucherRowViewModel(rows) {
+	var self = this;
+
+	self.id = ko.observable();
+	self.type = ko.observable();
+	self.description = ko.observable();
+	self.cr_amount = ko.observable();
+	self.dr_amount = ko.observable();
+	self.account = ko.observable();
+
 }
