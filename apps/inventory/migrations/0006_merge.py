f4d2dc7be5b1b7df3c58f5e031a78f09caa1c81f
Dipesh Acharya <xtranophilist@gmail.com>
2015-12-18 20:21:28 +0545

    mm

---
+++ apps/inventory/migrations/0006_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('inventory', '0005_sale_credit'),
+        ('inventory', '0003_auto_20151218_1857'),
+    ]
+
+    operations = [
+    ]
