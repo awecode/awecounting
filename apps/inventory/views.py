a4793092e4563e6c4eac3451edb0e7a3178b6165
Dipesh Acharya <xtranophilist@gmail.com>
2015-12-19 15:48:48 +0545

    mf

--- apps/inventory/views.py
+++ apps/inventory/views.py
@@ -153,7 +153,7 @@ def purchase(request, id=None):
         purchase = get_object_or_404(Purchase, id=id)
         scenario = 'Update'
     else:
-        purchase = Purchase(date=datetime.datetime.now().date(), company=request.company)
+        purchase = Purchase(company=request.company)
         scenario = 'Create'
     data = PurchaseSerializer(purchase).data
     return render(request, 'purchase-form.html', {'data': data, 'scenario': scenario, 'purchase': purchase})
