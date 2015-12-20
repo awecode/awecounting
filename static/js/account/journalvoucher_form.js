749b622aeeacd004133aaa91d11fe9382cf73926
Roshan Shrestha <roshanshrestha01@gmail.com>
2015-12-20 17:59:44 +0545

    journal voucher few validation

--- static/js/account/journalvoucher_form.js
+++ static/js/account/journalvoucher_form.js
@@ -5,23 +5,55 @@ $(document).ready(function () {
 });
 
 function JournalVoucherViewModel(data){
-	var self = this
+	var self = this;
 
 	self.id =  ko.observable();
 	self.voucher_no = ko.observable();
 	self.date = ko.observable();
 	self.narration = ko.observable();
 	self.status = ko.observable();
+
+    $.ajax({
+        url: '/ledger/api/account/?format=json',
+        dataType: 'json',
+        async: false,
+        success: function (data) {
+            self.accounts = ko.observableArray(data);
+        }
+    });
+
     self.table_view = new TableViewModel({rows: data.rows}, JournalVoucherRowViewModel);
 
     for (var k in data)
         self[k] = ko.observable(data[k]);
+
+    self.total_dr_amount = ko.computed(function() {
+        total = 0;
+        for (o in self.table_view.rows()) {
+            if (self.table_view.rows()[o].dr_amount()) {
+                total += parseInt(self.table_view.rows()[o].dr_amount());
+            };
+        }
+        return total;
+    });
+
+    self.total_cr_amount = ko.computed(function() {
+        total = 0;
+        for (o in self.table_view.rows()) {
+            if (self.table_view.rows()[o].cr_amount()) {
+                total += parseInt(self.table_view.rows()[o].cr_amount());
+            };
+        }
+        return total;
+    });
+
 
 }
 
 function JournalVoucherRowViewModel(row) {
 	var self = this;
 
+    self.account_type = ko.observableArray(['Cr','Dr']);
 	self.id = ko.observable();
 	self.type = ko.observable();
 	self.description = ko.observable();
@@ -32,4 +64,20 @@ function JournalVoucherRowViewModel(row) {
     for (var k in row)
         self[k] = ko.observable(row[k]);
 
+    self.is_dr = ko.computed(function() {
+        if(self.type() == "Dr")
+            return true
+        return false
+    });
+
+    self.is_cr = ko.computed(function() {
+        if(self.type() == "Cr")
+            return true
+        return false
+    });
+
+    self.type.subscribe(function(type) {
+        self.dr_amount('');
+        self.cr_amount('');
+    }); 
 }
