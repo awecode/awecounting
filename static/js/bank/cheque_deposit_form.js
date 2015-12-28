f558a8ce7d5d64c89394a8aeb22866957312743b
Roshan Shrestha <roshanshrestha01@gmail.com>
2015-12-28 14:51:00 +0545

    View Model and data-bind for cheque_deposit

--- static/js/bank/cheque_deposit_form.js
+++ static/js/bank/cheque_deposit_form.js
@@ -12,13 +12,23 @@ function ChequeDepositViewModel(data) {
     self.date = ko.observable();
     self.narration = ko.observable();
     self.status = ko.observable();
-
+    self.benefactor = ko.observable();
+    self.bank_account = ko.observable()
     $.ajax({
-        url: '/ledger/api/account/?format=json',
+        url: '/ledger/api/bank_account/account.json/',
         dataType: 'json',
         async: false,
         success: function (data) {
-            self.accounts = ko.observableArray(data);
+            self.bank_account_array = ko.observableArray(data);
+        }
+    });
+
+    $.ajax({
+        url: '/ledger/api/account.json',
+        dataType: 'json',
+        async: false,
+        success: function (data) {
+            self.benefactor_array = ko.observableArray(data);
         }
     });
 
@@ -26,61 +36,6 @@ function ChequeDepositViewModel(data) {
 
     for (var k in data)
         self[k] = ko.observable(data[k]);
-
-    self.total_dr_amount = ko.computed(function () {
-        total = 0;
-        for (o in self.table_view.rows()) {
-            if (self.table_view.rows()[o].dr_amount()) {
-                total += parseInt(self.table_view.rows()[o].dr_amount());
-            }
-            ;
-        }
-        return total;
-    });
-
-    self.total_cr_amount = ko.computed(function () {
-        total = 0;
-        for (o in self.table_view.rows()) {
-            if (self.table_view.rows()[o].cr_amount()) {
-                total += parseInt(self.table_view.rows()[o].cr_amount());
-            }
-            ;
-        }
-        return total;
-    });
-
-    self.check_description = function () {
-        arr = [];
-        for (i in self.table_view.rows()) {
-            arr.push(self.table_view.rows()[i].description());
-        }
-        if ($.inArray("", arr) >= 0 || $.inArray(undefined, arr) >= 0)
-            return true
-        return false
-    }
-
-    self.add_row = function (element, viewModel) {
-        $(element).blur();
-        var type;
-        var dr_amount;
-        var cr_amount;
-        var diff = self.total_dr_amount() - self.total_cr_amount();
-        if (diff > 0) {
-            type = 'Cr';
-            dr_amount = 0;
-            cr_amount = diff;
-        } else {
-            type = 'Dr';
-            cr_amount = 0;
-            dr_amount = (-1) * diff;
-        }
-
-        if ($(element).closest("tr").is(":nth-last-child(2)") && self.total_dr_amount() != self.total_cr_amount())
-            self.table_view.rows.push(new ChequeDepositRowViewModel({type: type, cr_amount: cr_amount, dr_amount: dr_amount}));
-
-        //focus on account selectize of next row
-        $(element).closest('tr').next('tr').find('td.account .selectize-control input').click();
-    }
 
     self.save = function (item, event) {
         if (self.total_cr_amount() !== self.total_dr_amount()) {
@@ -123,31 +78,14 @@ function ChequeDepositViewModel(data) {
 function ChequeDepositRowViewModel(row) {
     var self = this;
 
-    self.account_type = ko.observableArray(['Dr', 'Cr']);
     self.id = ko.observable();
-    self.type = ko.observable();
-    self.description = ko.observable();
-    self.cr_amount = ko.observable();
-    self.dr_amount = ko.observable();
-    self.account = ko.observable();
+    self.cheque_number = ko.observable();
+    self.cheque_date = ko.observable();
+    self.drawee_bank = ko.observable();
+    self.drawee_bank_address = ko.observable();
+    self.amount = ko.observable();
 
     for (var k in row)
         self[k] = ko.observable(row[k]);
 
-    self.is_dr = ko.computed(function () {
-        if (self.type() == "Dr")
-            return true
-        return false
-    });
-
-    self.is_cr = ko.computed(function () {
-        if (self.type() == "Cr")
-            return true
-        return false
-    });
-
-    self.type.subscribe(function (type) {
-        self.dr_amount(0);
-        self.cr_amount(0);
-    });
 }
