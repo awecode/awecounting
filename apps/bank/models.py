d8da2e90db41075c0749053fc77c5544ad00e457
Roshan Shrestha <roshanshrestha01@gmail.com>
2016-01-03 12:02:05 +0545

    File class in models.py

--- apps/bank/models.py
+++ apps/bank/models.py
@@ -180,3 +180,9 @@ class ElectronicFundTransferInRow(models.Model):
 
     def get_voucher_no(self):
         return self.electronic_fund_transfer_in.id
+
+
+class File(models.Model):
+    attachment = models.FileField(upload_to='cheque_payments/%Y/%m/%d', blank=True, null=True)
+    narration = models.TextField(null=True, blank=True)
+    cheque_deposit = models.ForeignKey(ChequeDeposit)
