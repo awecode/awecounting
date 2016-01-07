32aba108013d75851ad2986b98f93624de64a5e4
Dipesh Acharya <xtranophilist@gmail.com>
2016-01-07 13:14:41 +0545

    Two customers of same company cant have same pan NO., fixed #22

--- apps/ledger/views.py
+++ apps/ledger/views.py
@@ -34,6 +34,11 @@ class PartyView(CompanyView):
     success_url = reverse_lazy('party_list')
     form_class = PartyForm
 
+    def get_form_kwargs(self):
+        kwargs = super(PartyView, self).get_form_kwargs()
+        kwargs['request'] = self.request
+        return kwargs
+
 
 class PartyList(PartyView, ListView):
     pass
