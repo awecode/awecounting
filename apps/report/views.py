4efb2588aa7e300e33cbcd594b24b25d093d0db0
Dipesh Acharya <xtranophilist@gmail.com>
2016-04-03 20:19:56 +0545

    Trial balance as json

--- apps/report/views.py
+++ apps/report/views.py
@@ -1,9 +1,24 @@
+import json
+from django.http import JsonResponse
 from django.shortcuts import render
-from apps.ledger.models import Category
+from apps.ledger.models import Category, Account
+
+
+class NodeEncoder(json.JSONEncoder):
+    def __init__(self, *args, **kwargs):
+        super(NodeEncoder, self).__init__(*args, **kwargs)
+
+    def iterencode(self, o, _one_shot=False):
+        return 1
+
+
+def handler(obj):
+    print(obj)
+    return 1
 
 
 class Node(object):
-    def __init__(self, model, depth=0):
+    def __init__(self, model, parent=None, depth=0):
         self.children = []
         self.model = model
         self.name = self.model.name
@@ -11,35 +26,43 @@ class Node(object):
         self.dr = 0
         self.cr = 0
         self.depth = depth
-        self.parent = None
+        self.parent = parent
         if self.type == 'Category':
             for child in self.model.children.all():
-                self.add_child(Node(child, depth=self.depth + 1))
+                print self.add_child(Node(child, parent=self, depth=self.depth + 1))
             for account in self.model.accounts.all():
-                self.add_child(Node(account, depth=self.depth + 1))
+                print self.add_child(Node(account, parent=self, depth=self.depth + 1))
+        if self.type == 'Account':
+            self.dr = self.model.current_dr or 0
+            self.cr = self.model.current_cr or 0
+        if self.parent:
+            self.parent.dr += self.dr
+            self.parent.cr += self.cr
 
     def add_child(self, obj):
-        self.children.append(obj)
-        obj.parent = self
+        self.children.append(obj.get_data())
+
+    def get_data(self):
+        data = {
+            'name': self.name,
+            'type': self.type,
+            'dr': self.dr,
+            'cr': self.cr,
+            'children': self.children
+        }
+        return data
 
     def __str__(self):
         return self.name
 
-    def process_children(self):
-        import ipdb
-
-        ipdb.set_trace()
-
-
 def trial_balance(request):
     root_categories = Category.objects.filter(company=request.company, parent=None)
-    root = []
+    root = {'items': []}
     for root_category in root_categories:
         node = Node(root_category)
-        root.append(node)
-    import ipdb
-
-    ipdb.set_trace()
+        root['items'].append(node.get_data())
+    print(type(root))
+    return JsonResponse(root)
     context = {
         'categories': root_categories,
         'root': root,
