352bd66b0a2b76991c49013dd9fc83716933c6a5
Dipesh Acharya <xtranophilist@gmail.com>
2016-04-04 19:55:48 +0545

    Report Setting model

--- apps/report/models.py
+++ apps/report/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from apps.users.models import Company
 
-# Create your models here.
+
+class ReportSetting(models.Model):
+    company = models.OneToOneField(Company, related_name='report_settings')
+    # Trial Balance
+    show_root_categories_only = models.BooleanField(False)
+    show_zero_balance_ledgers = models.BooleanField(False)
+    show_zero_balance_categories = models.BooleanField(False)
+    hide_all_ledgers = models.BooleanField(False)
+    show_ledgers_only = models.BooleanField(False)
