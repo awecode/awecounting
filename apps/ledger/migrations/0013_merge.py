c021205dd82413c910bb8672ce557fbce901f2e1
Dipesh Acharya <xtranophilist@gmail.com>
2016-04-10 16:58:32 +0545

    merge migrations

---
+++ apps/ledger/migrations/0013_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ledger', '0012_auto_20160410_1650'),
+        ('ledger', '0012_auto_20160404_1955'),
+    ]
+
+    operations = [
+    ]
