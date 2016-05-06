37cb75c09d75f3cc510eabe9384efe61205867c8
Roshan Shrestha <roshanshrestha01@gmail.com>
2016-05-06 15:47:47 +0545

    django 1.9 depricated warning fix

--- awecounting/urls.py
+++ awecounting/urls.py
@@ -2,13 +2,16 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from rest_framework.authtoken import views
+from apps.inventory import views as inventory_views
+from apps.users import views as users_views
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^i18n/', include('django.conf.urls.i18n')),
-    url(r'^$', 'apps.inventory.views.index', name='home'),
-    url(r'^demo/$', 'apps.users.views.demo_login', name='demo'),
+    url(r'^$', inventory_views.index, name='home'),
+    url(r'^demo/$', users_views.demo_login, name='demo'),
 
     url(r'^users/', include('apps.users.urls', namespace='users')),
     url(r'^share/', include('apps.share.urls', namespace='share')),
@@ -23,10 +26,10 @@ urlpatterns = [
     url(r'^njango/', include('njango.urls')),
 
     url(r'^api-token-auth/', views.obtain_auth_token)
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += [
-        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
-            'document_root': settings.MEDIA_ROOT})
-    ]
+# if settings.DEBUG:
+    # urlpatterns += [
+    #     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+    #         'document_root': settings.MEDIA_ROOT})
+    # ]
