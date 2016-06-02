9ebccf881e6b835eb5f0f363277deba842f9d9e6
Dipesh Acharya <xtranophilist@gmail.com>
2016-06-02 13:11:00 +0545

    Trading Account

--- apps/report/views.py
+++ apps/report/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 from apps.ledger.models import Category, Node
 from apps.report.forms import ReportSettingForm
 from apps.report.models import ReportSetting
-from awecounting.utils.helpers import save_qs_from_ko
+from awecounting.utils.helpers import save_qs_from_ko, get_dict
 from awecounting.utils.mixins import group_required, SuperOwnerMixin, UpdateView
 
 
@@ -44,7 +44,6 @@ def save_report_settings(request):
     return JsonResponse(save_qs_from_ko(ReportSetting, filter_kwargs, request.body))
 
 
-
 class ReportSettingUpdateView(SuperOwnerMixin, UpdateView):
     model = ReportSetting
     form_class = ReportSettingForm
@@ -60,3 +59,35 @@ class ReportSettingUpdateView(SuperOwnerMixin, UpdateView):
         context['setting'] = 'ReportSetting'
         return context
 
+
+def get_subnode(node, name):
+    return get_dict(node['nodes'], 'name', name)
+
+def trading_account(request):
+    rows = []
+    data = get_trial_balance_data(request.company)
+    income = get_subnode(data, 'Income')
+    sales = get_subnode(income, 'Sales')
+    rows.append(('Sales', sales['cr']))
+    expenses = get_subnode(data, 'Expenses')
+    purchases = get_subnode(expenses, 'Purchase')
+    rows.append(('(Purchases)', purchases['dr']))
+    direct_expenses = get_subnode(expenses, 'Direct Expenses')
+    rows.append(('(Direct Expenses)', direct_expenses['dr']))
+    rows.append(('Gross Profit', float(sales['cr']) - float(purchases['dr'])-float(direct_expenses['dr']), 'ul'))
+    return render(request, 'trading_account.html', {'data': data, 'rows': rows})
+
+
+def profit_loss(request):
+    rows = []
+    data = get_trial_balance_data(request.company)
+    income = get_subnode(data, 'Income')
+    sales = get_subnode(income, 'Sales')
+    rows.append(('Sales', sales['cr']))
+    expenses = get_subnode(data, 'Expenses')
+    purchases = get_subnode(expenses, 'Purchase')
+    rows.append(('(Purchases)', purchases['dr']))
+    direct_expenses = get_subnode(expenses, 'Direct Expenses')
+    rows.append(('(Direct Expenses)', direct_expenses['dr']))
+    rows.append(('Gross Profit', float(sales['cr']) - float(purchases['dr'])-float(direct_expenses['dr']), 'ul'))
+    return render(request, 'profit_loss.html', {'data': data, 'rows': rows})
