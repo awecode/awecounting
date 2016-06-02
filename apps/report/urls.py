64443e7b5bc9adb13a082baea73fdf5e3c302304
Dipesh Acharya <xtranophilist@gmail.com>
2016-06-02 14:29:32 +0545

    Basic balance sheet

--- apps/report/urls.py
+++ apps/report/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+
 import views
 
 web_urls = [
@@ -8,7 +9,7 @@ web_urls = [
     url(r'^trial_balance.json$', views.trial_balance_json, name='trial_balance_json'),
     url(r'^trading_account/$', views.trading_account, name='trading_account'),
     url(r'^profit_loss/$', views.profit_loss, name='profit_loss'),
-
+    url(r'^balance_sheet/$', views.balance_sheet, name='balance_sheet'),
 ]
 
 urlpatterns = web_urls
