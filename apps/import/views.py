b6f6124a4512acbae7566ef947640233be4b2bc1
Roshan Shrestha <roshanshrestha01@gmail.com>
2016-06-14 17:19:17 +0545

    openpyxl open xls file in request.POST

--- apps/import/views.py
+++ apps/import/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from forms import ImportDebtor
+from openpyxl import load_workbook
 
 def import_debtor_tally(request):
+    if request.POST:
+        form = ImportDebtor(request.POST, request.FILES)
+        if form.is_valid():
+            file = request.FILES['file']
+            wb = load_workbook(file)
+            sheet = wb.worksheets[0]
+            import ipdb
+            ipdb.set_trace()
     form = ImportDebtor()
     return render(request, 'import/import_debtor_tally.html', {'form': form})
 # Create your views here.
