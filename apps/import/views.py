c832b144f459e3bf025a86549d25d3d28df2acd1
Roshan Shrestha <roshanshrestha01@gmail.com>
2016-06-15 13:01:42 +0545

    import debtor tally

--- apps/import/views.py
+++ apps/import/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from awecounting.utils.helpers import zero_for_none
+from ..ledger.models import Party, Account
 from forms import ImportDebtor
 from openpyxl import load_workbook
+
+def xls_debtor_tally(row):
+    dct = {}
+    header = {'A': 'particulars','B': 'debit','C': 'credit'}
+    for cell in row:
+        dct[header.get(cell.column)] = cell.value
+    return dct
+
 
 def import_debtor_tally(request):
     if request.POST:
@@ -9,8 +19,25 @@ def import_debtor_tally(request):
             file = request.FILES['file']
             wb = load_workbook(file)
             sheet = wb.worksheets[0]
-            import ipdb
-            ipdb.set_trace()
+            rows = tuple(sheet.iter_rows())
+            for row in rows[5:]:
+                params = xls_debtor_tally(row)
+                if type(params.get('debit')) != str and type(params.get('debit')) != str:
+                    if 'new_party' in request.POST:
+                        party = Party.objects.create(name=params.get('particulars'), company=request.company)
+                    else:
+                        party, party_created = Party.objects.get_or_create(name=params.get('particulars'), company=request.company)
+                    # account = Account.objects.get(name="Opening Balance Difference", category__name="Opening Balance Difference", company=request.company)
+
+                    party.customer_account.opening_cr = zero_for_none(params.get('credit'))
+                    # account.opening_dr = zero_for_none(params.get('credit'))
+
+                    party.customer_account.opening_dr = zero_for_none(params.get('debit'))
+                    # account.opening_cr = zero_for_none(params.get('debit'))
+
+                    # account.save()
+                    party.customer_account.save()
+                    party.save()
     form = ImportDebtor()
     return render(request, 'import/import_debtor_tally.html', {'form': form})
-# Create your views here.
+
