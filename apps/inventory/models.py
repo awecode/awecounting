9401c817d407f72aa6c06ad6ad62c269d9e60a56
Roshan Shrestha <roshanshrestha01@gmail.com>
2016-08-17 18:39:55 +0545

    BSDateField for JournalEntry in inventory app

--- apps/inventory/models.py
+++ apps/inventory/models.py
@@ -11,6 +11,7 @@ from ..users.models import Company
 from awecounting.utils.helpers import none_for_zero, zero_for_none
 from ..users.signals import company_creation
 from mptt.models import MPTTModel, TreeForeignKey
+from njango.fields import BSDateField
 
 
 class Unit(models.Model):
@@ -217,7 +218,7 @@ class Item(models.Model):
 
 
 class JournalEntry(models.Model):
-    date = models.DateField()
+    date = BSDateField()
     content_type = models.ForeignKey(ContentType, related_name='inventory_journal_entries')
     model_id = models.PositiveIntegerField()
     creator = GenericForeignKey('content_type', 'model_id')
