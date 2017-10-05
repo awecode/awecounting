5a28f09b248587acb6832b36585d2f9110ecacca
Roshan Shrestha <roshanshrestha01@gmail.com>
2017-10-05 12:45:53 +0545

    local_setting_sample.py modified

--- awecounting/local_settings_sample.py
+++ awecounting/local_settings_sample.py
@@ -2,7 +2,7 @@ from settings import *
 
 SECRET_KEY = '=s0$)ng6s4x@tt=e+v3hygikjuwn3d_m1ihz$m07e(g#bhj)pp'
 
-ADMINS = [('Dipesh Acharya', 'xxtranophilist@gmail.com')]
+ADMINS = [('Dipesh Acharya', 'xtranophilist@gmail.com')]
 SERVER_EMAIL = 'webmaster@xawecounting.com'
 
 DEBUG = True
@@ -14,29 +14,21 @@ MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
 MEDIA_URL = '/media/'
 
 INSTALLED_APPS += (
-    'debug_toolbar',
+    # 'debug_toolbar',
 )
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'awecounting',
-#         'USER': 'awecounting',
-#         'PASSWORD': 'password',
-#         'HOST': '',
-#         'PORT': '',
-#         'ATOMIC_REQUESTS': True,
-#     }
-# }
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dbname',
+        'USER': 'awecounting',
+        'PASSWORD': '',
+        'HOST': 'db',
+        'PORT': '',
     }
 }
 
-ALLOWED_HOSTS = ['beta.awecounting.com', 'awecounting.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['beta.awecounting.com', 'awecounting.com', 'www.awecounting.com', 'localhost', '127.0.0.1']
 
 TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
 
